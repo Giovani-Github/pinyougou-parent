814c6e3dd2122c9e0cba0cdbc262736dc53a518c
giovani <15219331778@163.com>
2018-08-11 17:23:56 +0800

    商品录入【品牌选择】、商品录入【扩展属性】 2018年8月11日17:23:49

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
 //控制层
-app.controller('goodsController', function ($scope, $controller, goodsService, uploadService, itemCatService) {
+app.controller('goodsController', function ($scope, $controller, goodsService, uploadService, itemCatService, typeTemplateService) {
 
     $controller('baseController', {$scope: $scope});//继承
 
@@ -162,7 +162,16 @@ app.controller('goodsController', function ($scope, $controller, goodsService, u
         );
     });
 
+    //模板ID选择后  更新品牌列表
+    $scope.$watch('entity.goods.typeTemplateId', function (newValue, oldValue) {
+        typeTemplateService.findOne(newValue).success(
+            function (response) {
+                $scope.typeTemplate = response;//获取类型模板
+                $scope.typeTemplate.brandIds = JSON.parse($scope.typeTemplate.brandIds);//品牌列表
+                $scope.entity.goodsDesc.customAttributeItems = JSON.parse($scope.typeTemplate.customAttributeItems);//扩展属性
+            }
+        );
+    });
 
 
-
-});	
+});
