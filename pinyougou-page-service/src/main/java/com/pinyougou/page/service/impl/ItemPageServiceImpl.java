8f53456fbe2c797585722825d62d907300424c30
giovani <15219331778@163.com>
2018-08-18 15:32:34 +0800

    商品详情页-数据显示 > 生成图片列表， 生成扩展属性列表， 生成规格列表， 生成商品类型面包屑 使用freemarker生成的详情页 2018年8月18日15:32:27

--- pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
+++ pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
@@ -3,6 +3,7 @@ package com.pinyougou.page.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.mapper.TbGoodsDescMapper;
 import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemCatMapper;
 import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbGoodsDesc;
@@ -32,6 +33,8 @@ public class ItemPageServiceImpl implements ItemPageService {
     private TbGoodsMapper goodsMapper;
     @Autowired
     private TbGoodsDescMapper goodsDescMapper;
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
 
     @Override
     public boolean genItemHtml(Long goodsId) {
@@ -42,9 +45,19 @@ public class ItemPageServiceImpl implements ItemPageService {
             //1.加载商品表数据
             TbGoods goods = goodsMapper.selectByPrimaryKey(goodsId);
             dataModel.put("goods", goods);
+
             //2.加载商品扩展表数据
             TbGoodsDesc goodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
             dataModel.put("goodsDesc", goodsDesc);
+
+            //3.商品分类
+            String itemCat1 = itemCatMapper.selectByPrimaryKey(goods.getCategory1Id()).getName();
+            String itemCat2 = itemCatMapper.selectByPrimaryKey(goods.getCategory2Id()).getName();
+            String itemCat3 = itemCatMapper.selectByPrimaryKey(goods.getCategory3Id()).getName();
+            dataModel.put("itemCat1", itemCat1);
+            dataModel.put("itemCat2", itemCat2);
+            dataModel.put("itemCat3", itemCat3);
+
             Writer out = new FileWriter(pagedir + goodsId + ".html");
             template.process(dataModel, out);
             out.close();
