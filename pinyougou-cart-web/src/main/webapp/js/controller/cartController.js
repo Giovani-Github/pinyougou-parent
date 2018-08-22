0dfa2b9d7d66f3f5c918bd0e6c951c2c584652ff
giovani <15219331778@163.com>
2018-08-22 15:17:34 +0800

    购物车前端代码 > 购物车数量增减与移除 2018年8月22日15:17:20

--- pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -8,4 +8,18 @@ app.controller('cartController', function ($scope, cartService) {
             }
         );
     }
+
+    //添加商品到购物车
+    $scope.addGoodsToCartList = function (itemId, num) {
+        cartService.addGoodsToCartList(itemId, num).success(
+            function (response) {
+                if (response.success) {
+                    $scope.findCartList();//刷新列表
+                } else {
+                    alert(response.message);//弹出错误提示
+                }
+            }
+        );
+    }
+
 });
